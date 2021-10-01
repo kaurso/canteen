8c4460a7c5c5155ec9ddc90ce4c528446994cca6
sarah <kaurso@wgc.school.nz>
2021-10-01 13:10:25 +1300

    added comment

--- index.php
+++ index.php
@@ -1,60 +1,66 @@
 <?php
-$con = mysqli_connect("localhost", "kaurso", "wildbrass60", "kaurso_cafe");
+$con = mysqli_connect("localhost", "kaurso", "wildbrass60", "kaurso_canteen");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
 else{
-    echo "connected to database";
+    echo " ";
 }
-/* Drinks Query*/
-/*Select DrinkID, Item FROM drinks*/
-$all_Drinks_query = "SELECT drinksID, item FROM drinks";
-$all_Drinks_result = mysqli_query($con, $all_Drinks_query);
 
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <title> Coffee shop </title>
-    <meta charset="utf-8">
-    <link rel='stylesheet' type='text/css' href='style.css'>
-</head>
+    <head>
+        <title> WGC CANTEEN </title>
+        <meta charset="utf-8">
+        <link rel='stylesheet' type='text/css' href='styling.css'>
+        <h1>WGC CANTEEN</h1>
+    </head>
 
-<body>
-<header>
-    <h1> Coffee Shop </h1>
-    <nav>
-        <ul>
-            <li> <a href="index.php"> HOME </a></li>
-            <li> <a href="drinks.php"> DRINKS </a></li>
-            <li> <a href="orders.php"> ORDERS </a></li>
-        </ul>
-    </nav>
-</header>
-<main>
-    <!--Drinks form-->
-    <form name= 'drinks form' id='drink_form' method='get' action='drinks.php'>
-        <select id='drink' name='drink'>
-            <!--options-->
-            <?php
-            while($all_Drinks_record = mysqli_fetch_assoc($all_Drinks_result)){
-                echo "<option value ='". $all_drinks_record['drinksID']."'>";
-                echo $all_Drinks_record['item'];
-                echo "</option>";
-            }
-            ?>
-        </select>
-        <input type='submit' name='drinks_button' value='Show me the drink information'>
-    </form>
-    <!--Orders form-->
-    <form name= 'orders form' id='order_form' method='get' action='orders.php'>
-        <select id='order' name='order'>
-            <!--options-->
-        </select>
-        <input type='submit' name='orders_button' value='Show me the orders information'>
-    </form>
-</main>
+    <body>
+    <div id="logo">
+        <a href="index.php"><img class="logo" src="images/WGC.png" width="125px" height="125px" alt="Image of logo"></a>
+    </div>
+        <header>
+            <nav>
+                <ul class="nav_links">
+                    <li> <a href='index.php'> HOME </a></li>
+                    <li> <a href='drinks.php'> DRINKS </a></li>
+                    <li> <a href='foods.php'> FOOD </a></li>
+                    <li> <a href='specials.php'> WEEKLY SPECIALS</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="contact.php"><button>Contact</button></a>
+        </header>
 
-</body>
+    <div class="container">
+        <div class="image">
+            <img src="images/canteen.jpg" width="600px" height="400px">
+        </div>
+        <div class="text">
+            <h2 id="headings">Our values</h2>
+            <p id="intro"> Here at the Wellington Girl's College, we make fresh, healthy and safe meals everyday!</p>
+            <p id="intro"> We believe that every kid should have access to affordable, healthy and fresh meals. That is
+                why, we have very low prices at our canteen!</p>
+            <p id="intro"> We have a variety of foods because we believe it is important that kids get a taste of every culture!
+                Our cuisines range from Mexican, English and Indian.</p>
+            <p id="intro"> </p>
+            <h2 id="headings">Daily Specials </h2>
+            <p id="intro"> Every day, we have a special combo meal. These combo meals are different everyday and come with a
+                drink and main serving. All special combo meals are only for $2.50. We keep our prices very cheap because
+                we know that not every child can afford expensive meals!</p>
+            <br><br>
+        </div>
+    </div>
+
+
+
+
+    </body>
+
+    <footer>
+        @Copyright Sarah Kaur 2021- All Right Reserved.
+    </footer>
 </html>
+
